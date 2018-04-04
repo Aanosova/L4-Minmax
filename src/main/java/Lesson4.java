480ce1f7444c2525f1cb0f4552e86b4447f6401d
Aanosova <anosova.aleksandra@gmail.com>
2018-04-04 22:35:32 +0300

    с заменой мин и макс

--- src/main/java/Lesson4.java
+++ src/main/java/Lesson4.java
@@ -16,16 +16,30 @@ public class Lesson4 {
             System.out.print(" " + array[i]);
         }
         int Max = -99;
+        int Indexmax = 0;
         for (int i = 0; i < array.length; i++) {
-            if (array[i] > Max & array[i] < 0) Max = array[i];
+            if (array[i] > Max & array[i] < 0){
+                Max = array[i]; Indexmax = i;}
         }
         System.out.println();
-        System.out.println("Максимальное отрицательное значение: " + Max);
+        System.out.println("Максимальное отрицательное значение: " + Max + "[" + Indexmax + "]");
 
         int min = 11;
+        int Indexmin = 0;
         for (int i = 0; i < array.length; i++) {
-            if (array[i] > 0 & array[i] < min) min = array[i];
+            if (array[i] > 0 & array[i] < min) {
+                min = array[i]; Indexmin = i;}
         }
-        System.out.println("Минимальное положительное значение: " + min);
+        System.out.println();
+        System.out.println("Минимальное положительное значение: " + min + "[" + Indexmin + "]");
+
+        int temp = array[Indexmin];
+        array[Indexmin] = array[Indexmax];
+        array [Indexmax] = temp;
+
+        System.out.println();
+        System.out.println(Arrays.toString(array));
+
+        }
+
     }
-}
